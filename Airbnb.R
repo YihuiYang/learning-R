251da46797fb4d78a94c523ccfb6065caf4fb372
YihuiYang <YihuiYang@users.noreply.github.com>
2016-05-13 17:56:10 +0800

    Add files via upload

---
+++ Airbnb.R
@@ -0,0 +1,63 @@
+# Exercise based on Airbnb data.
+# Data source: https://www.kaggle.com/c/airbnb-recruiting-new-user-bookings/data
+# Created 2016-05-02
+
+library(dplyr)
+library(ggplot2)
+
+getwd()
+setwd("/Users/Teradata/Documents/Learning_R/Airbnb")
+dir()
+sink("Airbnb.txt", append=T, split=T)
+
+
+# 1. Import data
+df_users <- read.csv("train_users_2.csv")
+df_sessions <- read.csv("sessions.csv")
+df_countries <- read.csv("countries.csv")
+df_age_gender_bkts <- read.csv("age_gender_bkts.csv")
+
+
+# 2. Exam data structure
+str(df_sessions)
+head(df_sessions)
+summary(df_sessions)
+
+str(df_users)
+summary(df_users)
+
+summary(df_countries)
+summary(df_age_gender_bkts)
+
+
+# 3. Explore session data
+# 3.1 Top action types
+df_sessions %>%
+  group_by(action_type) %>%
+  summarize(n_action_type=n()) %>%
+  arrange(desc(n_action_type))
+
+# 3.2 Mapping of action details
+df_sessions %>%
+  group_by(action_type, action, action_detail) %>%
+  summarise(n_records=n()) %>%
+  arrange(desc(n_records)) %>%
+  write.csv(file="action_map.csv")
+
+
+# 4. Explore user data
+# 4.1 User ID
+df_users %>%
+  summarize(cnt_records=n(), cnt_unique_users=n_distinct(id)) %>%
+  mutate(id_is_unique = (cnt_records == cnt_unique_users))
+# There are 213,451 observations. Data is unique on id column.
+
+# 4.2 Age
+df_users %>%
+  select(age) %>%
+  arrange(desc(age)) %>%
+  as.matrix() %>%
+  quantile(probs=c(0, 0.1, 0.25, 0.5, 0.75, 0.9, 1), na.rm=T)
+
+
+
